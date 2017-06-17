034722b4cfc30795263805f82980660abeffabb8
Juan Romo <jromo@mavs.uta.edu>
2017-06-16 23:38:24 -0500

    Base home screen added

--- app/src/main/java/com/romo/reminders/ui/HomeActivity.java
+++ app/src/main/java/com/romo/reminders/ui/HomeActivity.java
@@ -1,13 +1,15 @@
-package com.romo.reminders;
+package com.romo.reminders.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.romo.reminders.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
